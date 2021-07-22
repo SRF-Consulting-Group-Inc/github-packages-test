e816b68ef7110bb36b7d0230388f5b74c4530bbd
Riley Nielsen <rnielsen@srfconsulting.com>
2021-07-22 14:39:31 -0500

    Add project files.

---
+++ github-packages-test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github_packages_test
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            Console.WriteLine(nameof(Test));
+        }
+    }
+}
